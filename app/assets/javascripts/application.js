2284124df83ce25a3535c90772c672471b500592
Ali Zaidi <msazaidi@gmail.com>
2018-10-11 21:00:53 -0700

    textarea is scrollable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$('#content').on('change keyup keydown paste cut','textarea', function (){
+	$(this).height(0).height(this.scrollHeight);
+}).find('textarea').change();
